dc3c7479f53129cd1d6c04913bfdcefc8da5c1a8
AryanSaini <09aryan.lucky@gmail.com>
2025-03-09 16:36:11 +0530

    add advance

---
+++ 07_adv-JS/explore1.js
@@ -0,0 +1,10 @@
+console.log(`will learn about asynchronous nature`);
+
+setTimeout(function () {
+  console.log(`THis is a set timeout function`);
+}, 3000);
+
+for (let i = 0; i < 7; i++) {
+  // console.log(i);
+}
+
